cad76d753cf1d4147dcd41da334dca0e6ae142a6
medakasbi <medakasbi@gmail.com>
2024-03-17 11:06:58 +0000

    Adding robotAML.cpp for the first time

---
+++ robotAML.cpp
@@ -0,0 +1,215 @@
+//ARDUINO OBSTACLE AVOIDING CAR//
+// Before uploading the code you have to install the necessary library//
+//AFMotor Library https://learn.adafruit.com/adafruit-motor-shield/library-install //
+//NewPing Library https://github.com/livetronic/Arduino-NewPing// 
+//Servo Library https://github.com/arduino-libraries/Servo.git //
+// To Install the libraries go to sketch >> Include Library >> Add .ZIP File >> Select the Downloaded ZIP files From the Above links //
+
+
+//#include <AFMotor.h>  
+#include <NewPing.h>
+#include <Servo.h> 
+
+#define TRIG_PIN A0
+#define ECHO_PIN A1
+#define MAX_DISTANCE 200 
+#define MAX_SPEED 150 // sets speed of DC  motors
+#define MAX_SPEED_OFFSET 10
+#define SCAN_NUM 5
+
+NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE); 
+
+AF_DCMotor motor1(1, MOTOR12_1KHZ); 
+AF_DCMotor motor2(2, MOTOR12_1KHZ);
+AF_DCMotor motor3(3, MOTOR34_1KHZ);
+AF_DCMotor motor4(4, MOTOR34_1KHZ);
+Servo myservo;   
+
+boolean goesForward=false;
+int distance = 100;
+int speedSet = 0;
+
+void setup() {
+  Serial.begin(9600);
+  myservo.attach(10);  
+  myservo.write(90); 
+  delay(2000);
+  distance = readPing();
+  Serial.write(distance);
+  delay(100);
+  distance = readPing();
+
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+}
+
+void loop() {
+ int distanceR = 0;
+ int distanceL =  0;
+ delay(40);
+ 
+ if(distance<=40)
+ {
+  moveStop();
+  delay(100);
+  moveBackward();
+  delay(300);
+  moveStop();
+  delay(200);
+  distanceR = lookRight(SCAN_NUM);
+  delay(200);
+  distanceL = lookLeft(SCAN_NUM);
+  delay(200);
+
+  if(distanceR>=distanceL)
+  {
+    turnRight();
+    moveStop();
+  }else
+  {
+    turnLeft();
+    moveStop();
+  }
+ }else
+ {
+  moveForward();
+ }
+ distance = readPing();
+}
+
+int lookRight(int n)
+{
+    int tab[n];
+    for(int i=1; i<n+1;i++)
+    {
+        myservo.write(90 - (i*90/n)); 
+        delay(200);
+        tab[i] = readPing();
+        delay(500);
+    }
+
+    
+    myservo.write(90); 
+    delay(300);
+    return Max(tab,n);
+
+}
+
+int lookLeft(int n)
+{
+   int tab[n];
+    for(int i=1; i<n+1;i++)
+    {
+        myservo.write(90 + i*90/n); 
+        delay(200);
+        tab[i] = readPing();
+        delay(500);
+    }
+
+    
+    myservo.write(90); 
+    delay(300);
+    return Max(tab,n);
+    
+}
+
+int readPing() { 
+  delay(70);
+  int cm = sonar.ping_cm();
+  if(cm==0)
+  {
+    cm = 200;
+  }
+  return cm;
+}
+
+void moveStop() {
+  motor1.run(RELEASE); 
+  motor2.run(RELEASE);
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
+  } 
+  
+void moveForward() {
+
+ if(!goesForward)
+  {
+    goesForward=true;
+    motor1.run(FORWARD);      
+    motor2.run(FORWARD);
+    motor3.run(FORWARD); 
+    motor4.run(FORWARD);     
+   for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=1) // slowly bring the speed up to avoid loading down the batteries too quickly
+   {
+    motor1.setSpeed(speedSet);
+    motor2.setSpeed(speedSet);
+    motor3.setSpeed(speedSet);
+    motor4.setSpeed(speedSet);
+    delay(5);
+   }
+  }
+}
+
+void moveBackward() {
+    goesForward=false;
+    motor1.run(BACKWARD);      
+    motor2.run(BACKWARD);
+    motor3.run(BACKWARD);
+    motor4.run(BACKWARD);  
+  for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=1) // slowly bring the speed up to avoid loading down the batteries too quickly
+  {
+    motor1.setSpeed(speedSet);
+    motor2.setSpeed(speedSet);
+    motor3.setSpeed(speedSet);
+    motor4.setSpeed(speedSet);
+    delay(5);
+  }
+}  
+
+void turnRight() {
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(BACKWARD);
+  motor4.run(BACKWARD);     
+  delay(400);
+  motor1.run(FORWARD);      
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);      
+} 
+ 
+void turnLeft() {
+  motor1.run(BACKWARD);     
+  motor2.run(BACKWARD);  
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);   
+  delay(400);
+  motor1.run(FORWARD);     
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
+}  
+
+
+int max2(int a, int b)
+{
+  return a>b?a:b;
+}
+int max3(int a, int b, int c)
+{
+  return max(a,max2(b,c));
+}
+
+
+int Max(int t[], int n) {
+  int i,max;
+    max = t[0];
+    for ( i = 1; i < n; i++) {
+        if (t[i] > max)   max = t[i];
+       }
+
+    return max;
+}
